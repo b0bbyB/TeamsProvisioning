3f0e67bca44b51097d5f1cd495251a859102b2e4
Bo Christian Bertelsen <bo.bertelsen@cph.dk>
2021-02-23 14:23:18 +0100

    debug comments added

--- Services/Template/getTemplate.js
+++ Services/Template/getTemplate.js
@@ -16,30 +16,36 @@ module.exports = function getTemplate(context, token, jsonTemplate,
         var template;   // The template as a JavaScript object
 
         // 1. Get ID of the SharePoint site where template files are stored
-        context.log('Debug: Connecting to  template URL ' + settings().TEMPLATE_SITE_URL);
+        context.log('Debug - getTemplate.js: Connecting to  template URL ' + settings().TEMPLATE_SITE_URL);
         getSiteId(context, token, settings().TENANT_NAME, 
             settings().TEMPLATE_SITE_URL)
         .then((siteId) => {
         // 2. Get the Graph API drive ID for the doc library where template files are stored
+        context.log('Debug - getTemplate.js: Trying to get Drive ID');
              return getDriveId(context, token, siteId, settings().TEMPLATE_LIB_NAME);
         })
         .then((driveId) => {
         // 3. Get the download URL for the template file
+        context.log('Debug - getTemplate.js: Drive ID ' + driveId);
+        context.log('Debug - getTemplate.js: Trying to download the file');
             return getDownloadUrl(context, token, driveId,
                 `${jsonTemplate}${settings().TEMPLATE_FILE_EXTENSION}`);
         })
         .then((downloadUrl) => {
         // 4. Get the contents of the template file
+        context.log('Debug - getTemplate.js: Get content of the file');
             return downloadDriveItem(context, token, downloadUrl);
         })
         .then((templateString) => {
 
         // 5. Parse the template; get owner's user ID
+        context.log('Debug - getTemplate.js: Trying to parse it and get owner user ID');
             template = JSON.parse(templateString.trimLeft());
             return getUserId (context, token, owner);
         })
         .then((ownerId) => {
         // 6. Add the per-team properties to the template
+        context.log('Debug - getTemplate.js: Trying to add the per-team properties to the template');
 
             template['displayName'] = displayName;
             template['description'] = description;
@@ -48,6 +54,7 @@ module.exports = function getTemplate(context, token, jsonTemplate,
             ];
 
         // 7. Return the finished template as a string
+        context.log('Debug - getTemplate.js: Return the finished template as a string');
             resolve(JSON.stringify(template));
         })
         .catch((ex) => {
