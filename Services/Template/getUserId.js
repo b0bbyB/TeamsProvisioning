18c33ba087a62e39a74481b073e2eb4c97efae5d
b0bbyB <47139534+b0bbyB@users.noreply.github.com>
2021-04-12 13:21:06 +0200

    Update getUserId.js

--- Services/Template/getUserId.js
+++ Services/Template/getUserId.js
@@ -4,14 +4,20 @@ module.exports = function getUserId(context, token, userPrincipalName) {
 
     return new Promise((resolve, reject) => {
 
+        context.log('Debug - getUserId.js: Started getUserId - dumping UPN var');
+        context.log(userPrincipalName);
+        context.log('Debug - getUserId.js: testing for at sign to see if UPN');
         if (userPrincipalName.indexOf('@') < 0) {
 
             // If there's no @ sign, assume this is already a GUID
+            context.log('Debug - getUserId.js: If there is no at sign, assume this is already a GUID');
             resolve(userPrincipalName);
             
         } else {
-
+            context.log('Debug - getUserId.js: No at sign, get GUID');
             const url = `https://graph.microsoft.com/v1.0/users/${userPrincipalName}`;
+            context.log('Debug - getUserId.js: Contacting graph');
+            context.log(url);
             try {
 
                 request.get(url, {
